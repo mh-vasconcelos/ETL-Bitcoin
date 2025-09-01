458b1f52eb60ed552e98da59a9c321fdc80b58e1
matheus <mhvvbfr@gmail.com>
2025-09-01 17:26:51 -0300

    Nova versão código

--- load_csv.py
+++ load_csv.py
@@ -1,9 +1,20 @@
 from req_bitcoin import request_bitcoin, dt
+import pandas as pd
 
 if __name__ == "__main__":
-  header=True
+  try:
+    data = pd.read_csv('bitcoin_saves.csv')
+    if len(data) > 0:
+       header=False
+  except:
+    header=True
+
   while True:
-      df = request_bitcoin(lim_time=10, timestamp=dt.now())
-      df.to_csv("bitcoin_saves.csv", mode="a", index=False, header=header)
-      header=False
-      print("Cotações inseridas com sucesso, maluco!!!!!!")
+      try:
+        df = request_bitcoin(lim_time=10, timestamp=dt.now())
+        df.to_csv("bitcoin_saves.csv", mode="a", index=False, header=header)
+        header=False
+        print("Cotações inseridas com sucesso, maluco!!!!!!")
+      except:
+         print("Erro ao inserir")
+         break
--- pipeline.py
+++
@@ -1,6 +0,0 @@
-from req_bitcoin import request_bitcoin, dt
-
-url = "https://api.coinbase.com/v2/prices/spot"
-timestamp = dt.now()
-df = request_bitcoin(url, lim_time=30, timestamp=timestamp)
-print(df)
